995763f0a5419830d938af9d0925a742869ca5f6
Chen <is.chenx@gmail.com>
2017-10-14 10:27:42 -0400

    got detection working

--- play.py
+++ play.py
@@ -1,4 +1,3 @@
-# Reading in frame by frame
 import pdb
 import skvideo.io
 import skvideo.datasets
@@ -7,9 +6,19 @@ import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
 #from pyqtgraph.ptime import time as ptime
 import time
+import numpy as np
+import cv2
+import pdb
+import os
 
+opencvpath = '/home/cx1111/Software/opencv/data/haarcascades'
+face_cascade = cv2.CascadeClassifier(os.path.join(opencvpath, 'haarcascade_frontalface_default.xml'))
+#help(face_cascade.detectMultiScale)
+eye_cascade = cv2.CascadeClassifier(os.path.join(opencvpath, 'haarcascade_eye.xml'))
+
+
+# Video and window
 videogen = skvideo.io.vreader('examplevideo.mp4')
-
 app = QtGui.QApplication([])
 ## Create window with GraphicsView widget
 win = pg.GraphicsLayoutWidget()
@@ -26,11 +35,62 @@ view.addItem(img)
 updateTime = time.time()
 fps = 0
 
+
+# flatframe = cv2.imread('presidents.jpg')
+# print(flatframe.shape)
+# grayflatframe = cv2.cvtColor(flatframe, cv2.COLOR_BGR2GRAY)
+# flatfaces = face_cascade.detectMultiScale(grayflatframe, 1.3, 5)
+# for x,y,w,he in flatfaces:
+#     flatframe = cv2.rectangle(flatframe,(x,y),(x+w,y+he),(255,0,0),2)
+#     pdb.set_trace()
+# shape should be height, width, color(480, 720, 3)
+
 for frame in videogen:
+
+
+    # Get the image frame
+    #frame = frame.transpose([1,0,2])#[:,-1::-1,:]
+    grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Debugging
+    # print(grayframe)
+    # pg.image(frame)
+    # pg.image(grayframe)
+    # pdb.set_trace()
+
+    # Detect faces and eyes
+    faces = face_cascade.detectMultiScale(grayframe, 1.3, 5)
+
+    for x,y,w,he in faces:
+
+        # Draw the face region
+        
+        #print(frame.shape)
+        #pdb.set_trace()
+
+        frame = cv2.rectangle(frame,(x,y),(x+w,y+he),(255,0,0),2)
+
+        # Regions of interest
+        roi_gray = grayframe[y:y+he, x:x+w]
+        roi_color = frame[y:y+he, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        for ex,ey,ew,eh in eyes:
+            #pdb.set_trace()
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+
+    # Shift because pyqt is weird
     frame = frame.transpose([1,0,2])[:,-1::-1,:]
+    # Display
     img.setImage(frame)
     #time.sleep(0.01)
     app.processEvents()
+
+
+
+
+    # Timer content
 
     now = time.time()
     fps2 = 1.0 / (now-updateTime)
@@ -38,3 +98,7 @@ for frame in videogen:
     fps = fps * 0.9 + fps2 * 0.1
     
     print "%0.1f fps" % fps
+
+    #pdb.set_trace()
+
+
